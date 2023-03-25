f244fa55eada166028ef6eefe3686e0ccfb5300f
Azul De Pol <azul.depol@gmail.com>
2023-03-25 21:16:19 +0000

    complete el ejercicio 13

--- JS/TP4-ejercicio13.js
+++ JS/TP4-ejercicio13.js
@@ -2,3 +2,36 @@
 Crear un objeto calculadora, con las propiedades y métodos necesarios para realizar las operaciones de suma, resta, multiplicación y división. Mostrar por pantalla 2 operaciones con cada método.
  */
 
+let calculadora = {
+    
+    sumar: function (number1, number2){
+        document.write(number1 + number2);
+    },
+    restar: function (number1, number2){
+        document.write(number1 - number2)
+        
+    },
+    multiplicar: function (number1, number2){
+        document.write(number1 * number2)
+        
+    },
+    dividir: function (number1, number2){
+        document.write(number1 / number2)
+    }
+}
+
+calculadora.sumar(3,4);
+document.write(`<br></br>`);
+calculadora.sumar(6,49);
+document.write(`<br></br>`);
+calculadora.restar(400,200);
+document.write(`<br></br>`);
+calculadora.restar(5,9);
+document.write(`<br></br>`);
+calculadora.multiplicar(3,4);
+document.write(`<br></br>`);
+calculadora.multiplicar(15,10);
+document.write(`<br></br>`);
+calculadora.dividir(15,3);
+document.write(`<br></br>`);
+calculadora.dividir(100,2);
