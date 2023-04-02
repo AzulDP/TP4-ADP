67fff3e0eb773360a53bece2c273f5e5c52889ad
Azul De Pol <azul.depol@gmail.com>
2023-04-02 15:00:36 +0100

    empeze a trabajar el ejercicio5

--- JS/TP4-ejercicio5.js
+++ JS/TP4-ejercicio5.js
@@ -15,3 +15,101 @@ La fecha actual resto anio de nacimiento
 
  */
 
+class Persona {
+#nombre;
+#edad;
+#DNI;
+#sexo;
+#peso;
+#altura;
+#anio;
+#generacion;
+
+constructor (nombre, edad, DNI, sexo, peso, altura, anio, generacion = ("Generacion Z", "Generacion Y","Generacion X", "Baby Boom", "Silent Generation")){
+    this.#nombre = nombre;
+    this.#edad = edad;
+    this.#DNI = DNI;
+    this.#sexo = sexo;
+    this.#peso = peso;
+    this.#altura = altura;
+    this.#anio = anio;
+    this.#generacion = generacion;
+}
+
+get getNombre (){  
+}
+set setNombre (nuevoNombre){
+    this.#nombre = nuevoNombre;
+}
+get getEdad (){
+}
+set setEdad (nuevaEdad){
+    this.#edad = nuevaEdad;
+}
+get getDNI (){
+    
+}
+set setDNI (nuevoDNI){
+    this.#DNI = nuevoDNI;
+}
+get getSexo (){
+}
+
+set setSexo (nuevoSexo){
+    this.#sexo = nuevoSexo;
+}
+
+get getPeso (){
+}
+set setPeso (nuevoPeso){
+    this.#peso = peso;
+}
+
+get getAltura (){
+    
+}
+set setAltura (nuevaAltura){
+    this.#altura = altura;
+}
+get getAnio(){
+}
+set setAnio (nuevoAnio){
+        this.#anio = nuevoAnio;
+    }
+get getGeneracion(){
+}
+set setGeneracion (nuevaGeneracion){
+        this.#generacion = nuevaGeneracion;
+    }
+
+    mostrarGeneracion(){
+document.write(`${this.nombre} pertenece a la ${this.#generacion}`);
+
+}
+
+esMayorDeEdad(){
+if (this.#edad <= 18) {
+    document.write(`${this.#nombre} es mayor de edad`);
+}
+}
+
+mostrarDatos(){
+    document.write(`<ul>
+    <li>Nombre:${this.#nombre}</li>
+    <li>Edad: ${this.#edad}</li>
+    <li>DNI: ${this.#DNI}</li>
+    <li>Sexo: ${this.#sexo}</li>
+    <li>Peso: ${this.#peso}</li>
+    <li>Altura: ${this.#altura}</li>
+    <li>Año de Nacimiento: ${this.#anio}</li>
+    <li>Generación: ${this.#generacion}</li>
+</ul>`);
+}
+
+generaDNI(){
+    let randomNumber = (Math.random() * 99999999) + 1;
+console.log(randomNumber);
+document.write(`Tu número de DNI es ${randomNumber}`);
+}
+}
+
