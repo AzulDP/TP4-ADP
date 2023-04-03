790d1fdb039bd79a2570f23289fa2bb0c5456d27
Azul De Pol <azul.depol@gmail.com>
2023-04-03 18:34:41 +0100

    termine de agregar los metodos y cree los dos objetos con nuevas propiedades y llamando a los metodos. ejercicio completo

--- JS/TP4-ejercicio8.js
+++ JS/TP4-ejercicio8.js
@@ -32,5 +32,18 @@ set setProfesion(nuevaProfesion){
 this.#profesion = nuevaProfesion;
 }
 
-
+saludar(){
+    document.write(`<p>Hola ${this.#nombre}!</p>`)
 }
+despedirse(){
+    document.write(`<p>Adiós ${this.#nombre}!</p>`)
+}
+}
+
+const persona1 = new Persona ("Felipe", "35", "hacker ético");
+console.log(persona1);
+persona1.saludar();
+
+const persona2 = new Persona ("Macarena", "28", "bailarina");
+console.log(persona2);
+persona2.saludar();
