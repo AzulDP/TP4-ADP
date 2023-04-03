d397d6f2f7bd772138ead57c8f4a1a16a25cdefe
Azul De Pol <azul.depol@gmail.com>
2023-04-03 18:44:17 +0100

    empeze a trabajar el ejercicio 9 agregando la primer clase

--- JS/TP4-ejercicio9.js
+++ JS/TP4-ejercicio9.js
@@ -1,3 +1,38 @@
 /*
 9- Crea una clase llamada "Animal" que tenga las propiedades "nombre" y "edad", y el método "emitirSonido". Luego, crea dos clases hijas llamadas "Perro" y "Gato" que hereden de "Animal" y tengan su propio método "emitirSonido". Finalmente, crea dos objetos, uno de la clase "Perro" y otro de la clase "Gato", y llama a sus métodos "emitirSonido" para verificar que cada animal emite el sonido adecuado. 
  */
+
+class Animal {
+    #nombre;
+    #edad;
+constructor (nombre, edad){
+    this.#nombre = nombre;
+    this.#edad = edad;
+}
+
+get getNombre(){
+return this.#nombre;
+}
+
+set setNombre(nuevoNombre){
+ this.#nombre = nuevoNombre;
+}
+
+get getEdad(){
+return this.#edad;
+}
+
+set setEdad(nuevaEdad){
+this.#edad = nuevaEdad;
+}
+
+mostrarInformacion(){
+    document.write(`${this.#nombre} ${this.#edad}`)
+}
+emitirSonido(){
+document.write(`<p>El sonido de ${this.#nombre} es </p>`)
+}
+}
+class Perro extends Animal{
+
+}
