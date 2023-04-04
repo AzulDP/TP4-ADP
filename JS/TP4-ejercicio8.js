32756e181ea9c01e8eedc371fd14a29199ded801
Azul De Pol <azul.depol@gmail.com>
2023-04-04 20:45:18 +0100

    hago el ultimo merge

--- JS/TP4-ejercicio8.js
+++ JS/TP4-ejercicio8.js
@@ -43,7 +43,9 @@ despedirse(){
 const persona1 = new Persona ("Felipe", "35", "hacker ético");
 console.log(persona1);
 persona1.saludar();
+persona1.despedirse();
 
 const persona2 = new Persona ("Macarena", "28", "bailarina");
 console.log(persona2);
 persona2.saludar();
+persona2.despedirse();
